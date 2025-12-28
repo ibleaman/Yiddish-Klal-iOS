65caf28db804ceefa1c415620fbc48d5454baf83
Isaac L. Bleaman <bleaman@berkeley.edu>
2025-12-28 05:43:21 -0800

    Initial Commit

---
+++ Yiddish Klal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yiddish Klal
+//
+//  Created by Isaac L. Bleaman on 12/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
